c03ae497ddd40556a8d00f299abb9c454145818c
jaylakhani2403 <23ce062@charusat.edu.in>
2025-02-19 22:11:50 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import Login from './components/login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      Hello
+  <Login></Login>
+     
     </>
   )
 }
