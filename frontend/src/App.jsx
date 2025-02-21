ce73ee0486de5286ea09825d5cb6d43dc50131a4
jaylakhani2403 <23ce062@charusat.edu.in>
2025-02-21 22:22:12 +0530

    delete files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import CreateTeam from "./components/CreateTeam";
-import TeamDashboard from "./components/TeamDashboard";
-import StudentPanel from "./components/studentdashbord";
-import JoinTeam from "./components/JoinTeam";
+
+import Dashboard from "./components/facultydashbord"
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<StudentPanel />} />
-      <Route path="/create-team" element={<CreateTeam />} />
-      <Route path="/join-team" element={<JoinTeam/>}/>
-      <Route path="/dashboard" element={<TeamDashboard />} />
+      <Route path="/" element={<Dashboard/>} />
     </Routes>
   );
 }
